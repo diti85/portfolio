e066eebc50234cd4033314372884a06c9f64f2c0
Endrit Basha <101580207+diti85@users.noreply.github.com>
2023-05-08 17:53:03 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_vh8j5y8",
+        "template_bpbn6p8",
         {
           from_name: form.name,
           to_name: "Endrit Basha",
